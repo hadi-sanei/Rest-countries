862169e77aca3f5ca8a2ab05be5b27af718a1b8d
hadi sanei <hadi.sanei.net@gmail.com>
2024-07-06 20:24:01 +0330

    add fontawesome package and font to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'Mobile': '375px',
+        'Desktop': '1400px'
+      },
+      colors: {
+        LightMode_Background: 'hsl(var(--Light-Mode-Background))',
+        DarkModeText_and_Light_Mode_Elements: 'hsl(var(--Dark-Mode-Text-and-Light-Mode-Elements))',
+        LightMode_Input: 'hsl(var(--Light-Mode-Input))',
+        LightMode_Text: 'hsl(var(--Light-Mode-Text))',
+        DarkMode_Background: 'hsl(var(--Dark-Mode-Background))',
+        DarkMode_Elements: 'hsl(var(--Dark-Mode-Elements))',
+      }
+    },
   },
   plugins: [],
 }
