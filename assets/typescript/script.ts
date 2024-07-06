8e6676caeec20e5b3b5164d18490da0c2897f7b8
hadi sanei <hadi.sanei.net@gmail.com>
2024-07-06 23:17:18 +0330

    completing the header section and adding dark mode

--- assets/typescript/script.ts
+++ assets/typescript/script.ts
@@ -1,1 +1,12 @@
-console.log('objects');
+const change_theme = document.getElementById('change-theme') as HTMLElement;
+if (document.cookie.includes('theme=dark')) {
+    document.body.classList.add('dark-theme');
+}
+change_theme.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        document.cookie = `theme=dark`;
+        return;
+    }
+    document.cookie = 'theme=dark;max-age=0';
+});
