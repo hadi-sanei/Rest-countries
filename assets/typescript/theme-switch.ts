12b7278005f2d4ef1e84524616a58c234069c31d
hadi sanei <hadi.sanei.net@gmail.com>
2024-07-26 10:21:06 +0330

    Fix:change button text in dark mode to 'Light Mode'

--- assets/typescript/theme-switch.ts
+++ assets/typescript/theme-switch.ts
@@ -3,13 +3,16 @@ export function theme_switch() {
     const change_theme = document.getElementById('change-theme') as HTMLElement;
     if (document.cookie.includes('theme=dark')) {
         document.body.classList.add('dark-theme');
+        change_theme.innerHTML = '<i class="fa-regular fa-sun text-base mx-1"></i> Light Mode';
     }
     change_theme.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme');
         if (document.body.classList.contains('dark-theme')) {
             document.cookie = `theme=dark;max-age=${3 * 24 * 60 * 60};path=/`;
+            change_theme.innerHTML = '<i class="fa-regular fa-sun text-base mx-1"></i> Light Mode';
             return;
         }
+        change_theme.innerHTML = '<i class="fa-regular fa-moon text-base mx-1"></i> Dark Mode';
         document.cookie = 'theme=dark;max-age=0;path=/';
     });
 }
